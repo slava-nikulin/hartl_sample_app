d923814a1d7190dd6948ba1dc3065b5002b117fc
slava-nikulin <unicorninthecorn@gmail.com>
2015-11-17 03:07:49 -0800

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/contact'
 
-  root 'static_pages/home'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
